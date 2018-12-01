afa6fe016e6093165bbb7c161a71b9ccb12fdbc4
sleepy_wolf <43232088+ceccs17d40@users.noreply.github.com>
2018-12-01 09:19:13 +0530

    Final commit
    Added name and roll number

---
+++ 4_bubble_sort.c
@@ -0,0 +1,39 @@
+// Pranay Prince
+// S3D
+// 40
+
+#include <stdio.h>
+void bsort(int arr[],int n)
+{
+	int i,j,temp;
+   	for(i=n-1;i>=0;i--)  
+	{   
+		for(j=0;j<i;j++)
+		{
+       		if(arr[j]>arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}		
+		}
+	}
+}
+int main(void)
+{
+    int arr[25],n,i;
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	printf("Enter elements:");
+	for(i=0;i<n;++i)
+	{
+		scanf("%d",&arr[i]);
+	}
+    	bsort(arr,n);
+   	printf("Sorted array: \n");
+	for(i=0;i<n;++i)
+	{
+		printf("%d\n",arr[i]);
+	}
+ 	return 0;
+}
